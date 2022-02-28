91d8a73a07c315941850f9d3dfb348b2cdec89b0
Maximiliano Narea <48288027+manarea@users.noreply.github.com>
2022-02-28 20:13:35 -0300

    General fix for existance of the target div, new banner added, selenium driver path error handle implemented.

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from webScrapper import WebScrapper
 from sys import stdout
+from banner import print_banner
+from selenium import webdriver
+
 
 if __name__ == "__main__":
-    with open("cursos_titulo.txt","r") as f:
-        courses = dict(map(lambda x: x.split(" ",1),f.readlines()))
-        f.close() 
+    print_banner(title="Titulo Scrap")
+
+    with open("cursos_titulo.txt", "r") as f:
+        courses = dict(map(lambda x: x.split(" ", 1), f.readlines()))
+        f.close()
     _WebScrapper = WebScrapper("https://buscacursos.uc.cl/")
     _WebScrapper.load_page()
-    with open("courses_list_curated.txt","w") as f:
+    with open("courses_list_curated.txt", "w") as f:
         courses_copy = courses.copy()
         for course in courses_copy.keys():
             _WebScrapper.send_input(course)
             _WebScrapper.submit()
-            # xD
             if not _WebScrapper.check_existance("bordeBonito"):
                 courses.pop(course)
             else:
                 stdout.write("Writing course\n")
-                f.write(f"{course}: {courses[course]}\n")
-                
-        _WebScrapper.finish()
-        stdout.write("New file was created\n")
+                f.write(f"{course}: {courses[course]}")
+    _WebScrapper.finish()
+    stdout.write("New file was created\n")
